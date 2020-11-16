145d428cbb12a8273a7f05239de0f268ef325f2e
Your Name <you@example.com>
2020-11-16 18:48:26 -0500

    [sqlzoo] Unit 6 complete

--- sqlzoo_answers.sql
+++ sqlzoo_answers.sql
@@ -1,24 +1,43 @@
 -- 6 JOIN
 -- 1.
-
+SELECT matchid, player
+FROM goal 
+  WHERE teamid = 'GER'
 -- 2.
-
+SELECT id,stadium,team1,team2
+  FROM game
+WHERE id = 1012;
 -- 3.
-
+SELECT player, teamid, stadium, mdate
+  FROM game JOIN goal ON (id=matchid)
+WHERE teamid = 'GER'
 -- 4.
-
+SELECT team1, team2, player
+FROM game JOIN goal on (id=matchid)
+WHERE player LIKE 'Mario%';
 -- 5.
-
+SELECT player, teamid, coach, gtime
+  FROM goal JOIN eteam ON teamid=id
+ WHERE gtime<=10
 -- 6.
-
+SELECT mdate, teamname
+FROM game JOIN eteam ON team1=eteam.id
+WHERE coach = 'Fernando Santos'
 -- 7.
-
+SELECT player
+FROM game JOIN goal ON id=matchid
+WHERE stadium = 'National Stadium, Warsaw'
 -- 8. - Skip this one
 
 -- 9.
-
+SELECT teamname, COUNT(player)
+  FROM eteam JOIN goal ON id=teamid
+GROUP BY teamname
 -- 10.
-
+SELECT stadium, COUNT(player) AS goals
+FROM game
+JOIN goal ON (id=matchid)
+GROUP BY stadium
 -- 11. - Skip this one
 
 -- 12. - Skip this one
