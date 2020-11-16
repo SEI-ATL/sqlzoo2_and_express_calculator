64d47cbd2dedf162ffb493acfa62f125a69f1912
Your Name <you@example.com>
2020-11-16 17:49:17 -0500

    finished

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ const ejsLayouts = require('express-ejs-layouts')
 app.set('view engine', 'ejs')
 
 
-app.get('/math/doubles/:number', (req, res) => {
+app.get('/math/double/:number', (req, res) => {
     // let number = :number;
     const number = req.params.number
-    res.render('doubles', {number})
+    res.render('double', {number})
 })
 app.get('/math/square/:number', (req, res) => {
-    let outputNumber = number * number
+    // let number = :number;
+    const number = req.params.number
+    res.render('square', {number})
+})
+app.get('/string/reverse/:yourString', (req, res) => {
+    const yourString = req.params.yourString
+    res.render('reverse', {yourString})
+})
+app.get('/string/upcase/:yourString', (req, res) => {
+    const yourString = req.params.yourString
+    res.render('upcase', {yourString})
 })
 
 
-
+function reverseString(str){
+    let newStr = str.split('')
+    newStr = newStr.rever()
+    newStr.join('')
+    return newStr
+}
 
 
 app.listen(1337, () =>{
