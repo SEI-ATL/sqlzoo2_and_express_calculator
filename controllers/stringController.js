c0f6434ca88d1da11ca32779501a8ba6190ddc6a
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-11-17 07:34:28 -0700

    string controller logic

--- controllers/stringController.js
+++ controllers/stringController.js
@@ -1,14 +1,16 @@
 const stringController = require('express').Router()
 
+stringController.get('/reverse/:string', (req, res) => {
+    const string = req.params.string
 
+    res.render('string/reverse', { initialString: string, reversedString: string.split(''). reverse().join('')})
+})
 
+stringController.get('upcase/:string', (req, res) => {
+    const string = req.params.string
 
-
-
-
-
-
-
+    res.render('string/upcase', { initialString: string, upcasedString: string.split(''). toUpperCase().join('')})
+})
 
 
 
