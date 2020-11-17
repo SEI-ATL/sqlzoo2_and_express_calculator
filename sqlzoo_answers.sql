66436f72922e5a87f560866318168d33c477c239
jtreeves <jackson.t.reeves@gmail.com>
2020-11-16 19:54:53 -0500

    [sql] Finish question 6

--- sqlzoo_answers.sql
+++ sqlzoo_answers.sql
@@ -1,37 +1,54 @@
 -- 6 JOIN
 -- 1.
-
+SELECT matchid, player
+FROM goal 
+WHERE teamid = 'GER'
 -- 2.
-
+SELECT id, stadium, team1, team2
+FROM game
+WHERE id = 1012
 -- 3.
-
+SELECT player, teamid, stadium, mdate
+FROM game JOIN goal ON id = matchid
+WHERE teamid = 'GER'
 -- 4.
-
+SELECT team1, team2, player
+FROM game JOIN goal ON id = matchid
+WHERE player LIKE 'Mario%'
 -- 5.
-
+SELECT player, teamid, coach, gtime
+FROM goal JOIN eteam ON teamid = id
+WHERE gtime <= 10
 -- 6.
-
+SELECT mdate, teamname
+FROM game JOIN eteam ON team1 = eteam.id
+WHERE coach = 'Fernando Santos'
 -- 7.
-
+SELECT player
+FROM goal JOIN game ON matchid = id
+WHERE stadium = 'National Stadium, Warsaw'
 -- 8. - Skip this one
-
 -- 9.
-
+SELECT teamname, COUNT(player)
+FROM eteam JOIN goal ON id = teamid
+GROUP BY teamname
 -- 10.
-
+SELECT stadium, COUNT(player)
+FROM game JOIN goal ON id = matchid
+GROUP BY stadium
 -- 11. - Skip this one
-
 -- 12. - Skip this one
-
 -- 13. - Skip this one
-
-
 
 -- 7 More JOIN operations
 -- 1.
-
+SELECT id, title
+FROM movie
+WHERE yr = 1962
 -- 2.
-
+SELECT yr
+FROM movie
+WHERE title = 'Citizen Kane'
 -- 3.
 
 -- 4.
@@ -81,7 +98,6 @@
 -- 10.
 
 
-
 -- 8+ Numeric Examples
 -- 1.
 
@@ -98,4 +114,3 @@
 -- 7.
 
 -- 8.
-
