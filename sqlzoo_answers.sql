e32507c76ab2f1041180c5ea98585925e1e35ee1
Your Name <you@example.com>
2020-11-16 20:51:24 -0500

    work on sqlzoo a few more to do

--- sqlzoo_answers.sql
+++ sqlzoo_answers.sql
@@ -48,21 +48,40 @@ GROUP BY stadium
 
 -- 7 More JOIN operations
 -- 1.
-
+SELECT id, title
+ FROM movie
+ WHERE yr=1962
 -- 2.
-
+SELECT yr
+FROM movie
+WHERE title = 'Citizen Kane'
 -- 3.
-
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE '%Star Trek%'
+ORDER BY yr
 -- 4.
-
+SELECT id
+FROM actor
+WHERE name LIKE 'Glenn Close'
 -- 5.
-
+SELECT id
+FROM movie
+WHERE title = 'Casablanca'
 -- 6.
-
+SELECT name
+FROM casting
+JOIN actor ON actorid=id
+WHERE movieid = 11768
 -- 7.
-
+SELECT name
+FROM casting
+JOIN actor ON actorid=id
+WHERE movieid = 10522
 -- 8.
-
+SELECT title
+FROM movie
+  JOIN casting ON id=movieid and actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford')
 -- 9.
 
 -- 10.
@@ -80,41 +99,87 @@ GROUP BY stadium
 
 -- 8 Using Null
 -- 1.
-
+SELECT name
+FROM teacher
+WHERE dept IS NULL
 -- 2.
-
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
 -- 3.
-
+SELECT teacher.name, dept.name
+FROM teacher LEFT JOIN dept
+ON (teacher.dept=dept.id)
 -- 4.
-
+SELECT teacher.name, dept.name
+FROM teacher RIGHT JOIN dept
+ON (teacher.dept=dept.id)
 -- 5.
-
+SELECT name, COALESCE(mobile, '07986 444 2266')
+FROM teacher
 -- 6.
-
+SELECT teacher.name, COALESCE(dept.name, 'None')
+FROM teacher LEFT JOIN dept
+ON teacher.dept = dept.id
 -- 7.
-
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher
 -- 8.
-
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher RIGHT JOIN dept ON teacher.dept = dept.id
+GROUP BY dept.name
 -- 9.
-
+SELECT teacher.name, CASE WHEN dept.id=1
+THEN 'Sci'
+WHEN dept.id=2
+THEN 'Sci'
+ELSE 'Art' END
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
 -- 10.
-
+SELECT teacher.name, CASE WHEN dept.id=1
+THEN 'Sci'
+WHEN dept.id=2
+THEN 'Sci'
+WHEN dept.id=3 
+THEN 'Art'
+ELSE 'None'
+END
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
 
 
 -- 8+ Numeric Examples
 -- 1.
-
+SELECT COUNT(name)
+FROM stops
 -- 2.
-
+SELECT id
+FROM stops
+WHERE name = 'Craiglockhart'
 -- 3.
-
+SELECT id, name
+FROM stops JOIN route
+ON id=stop
+WHERE company = 'LRT' AND num = 4
 -- 4.
-
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
 -- 5.
-
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop=149
 -- 6.
-
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart'AND stopb.name = 'London Road'
 -- 7.
-
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=115 AND b.stop =137
 -- 8.
 
