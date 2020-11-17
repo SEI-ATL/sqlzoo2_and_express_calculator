35ab0047c07b2bb9e479fcbc7308ecf828e97d59
jtreeves <jackson.t.reeves@gmail.com>
2020-11-16 20:41:06 -0500

    [sql] Finish majority of question 7

--- sqlzoo_answers.sql
+++ sqlzoo_answers.sql
@@ -50,23 +50,64 @@ SELECT yr
 FROM movie
 WHERE title = 'Citizen Kane'
 -- 3.
-
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE 'Star Trek%'
+ORDER BY yr
 -- 4.
-
+SELECT id
+FROM actor
+WHERE name = 'Glenn Close'
 -- 5.
-
+SELECT id
+FROM movie
+WHERE title = 'Casablanca'
 -- 6.
-
+SELECT name
+FROM actor
+JOIN casting ON actor.id = casting.actorid
+JOIN movie ON casting.movieid = movie.id
+WHERE movieid =
+(SELECT id
+FROM movie
+WHERE title = 'Casablanca')
 -- 7.
-
+SELECT name
+FROM actor
+JOIN casting ON actor.id = casting.actorid
+JOIN movie ON casting.movieid = movie.id
+WHERE movieid =
+(SELECT id
+FROM movie
+WHERE title = 'Alien')
 -- 8.
-
+SELECT title
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON casting.actorid = actor.id
+WHERE name = 'Harrison Ford'
 -- 9.
-
+SELECT title
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON casting.actorid = actor.id
+WHERE name = 'Harrison Ford'
+AND ord <> 1
 -- 10.
-
+SELECT title, name
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON casting.actorid = actor.id
+WHERE yr = 1962
+AND ord = 1
 -- 11.
-
+SELECT yr, COUNT(title)
+FROM movie
+JOIN casting ON movie.id = movieid
+JOIN actor ON actorid = actor.id
+WHERE name = 'Rock Hudson'
+GROUP BY yr
+HAVING COUNT(title) > 2
 -- 12.
 
 -- 13.
