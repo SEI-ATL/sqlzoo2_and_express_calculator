904838b6a41e44dde43453e9d7aac59b13de8ab3
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-16 20:11:34 -0500

    [SQLZOO] Completed

--- sqlzoo_answers.sql
+++ sqlzoo_answers.sql
@@ -179,18 +179,51 @@ FROM teacher LEFT JOIN dept ON (teacher.dept = dept.id)
 
 -- 8+ Numeric Examples
 -- 1.
-
+SELECT A_STRONGLY_AGREE
+FROM nss
+WHERE question='Q01'
+      AND institution='Edinburgh Napier University'
+      AND subject='(8) Computer Science'
 -- 2.
-
+SELECT institution, subject
+FROM nss
+WHERE question='Q15'
+   AND score >= 100;
 -- 3.
-
+SELECT institution,score
+FROM nss
+WHERE question='Q15'
+      AND subject='(8) Computer Science'
+      AND score < 50;
 -- 4.
-
+SELECT subject, SUM(response) 
+FROM nss 
+WHERE question = 'Q22'
+GROUP BY subject HAVING subject 
+IN('(8) Computer Science', '(H) Creative Arts and Design');
 -- 5.
-
+SELECT subject, SUM((response*A_STRONGLY_AGREE)/100)
+FROM nss
+WHERE question = 'Q22'
+GROUP BY subject HAVING subject 
+IN('(8) Computer Science', '(H) Creative Arts and Design');
 -- 6.
-
+SELECT subject, ROUND(SUM(A_STRONGLY_AGREE * response) / SUM(response))
+FROM nss
+WHERE question='Q22'
+GROUP BY subject
+HAVING subject IN('(8) Computer Science',
+'(H) Creative Arts and Design');
 -- 7.
-
+SELECT institution,ROUND(SUM(score * response)/SUM(response))
+FROM nss
+WHERE question='Q22'
+      AND (institution LIKE '%Manchester%')
+GROUP BY institution
 -- 8.
-
+SELECT institution, SUM(sample) AS sample_n, 
+SUM(CASE WHEN subject = '(8) Computer Science' THEN sample
+ELSE 0 END) AS computing_n
+FROM nss
+WHERE question='Q01' AND (institution LIKE '%Manchester%')
+GROUP BY institution;
