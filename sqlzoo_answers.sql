a31289f99e07e4f0541bf9b3354ef28946288eb0
Thomas Gilbert <tcgilbert94@gmail.com>
2020-11-17 08:43:34 -0500

    sql 6 and 7

--- sqlzoo_answers.sql
+++ sqlzoo_answers.sql
@@ -1,24 +1,61 @@
 -- 6 JOIN
 -- 1.
-
+SELECT COUNT(*)
+FROM stops
 -- 2.
-
+SELECT id
+FROM stops
+WHERE name = 'Craiglockhart'
 -- 3.
-
+SELECT id, name
+FROM stops
+    JOIN route ON id=stop
+WHERE company = 'LRT' AND num=4
 -- 4.
-
+SELECT company, num, COUNT(*) AS visits
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING visits=2
 -- 5.
-
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop=149
 -- 6.
-
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart'AND stopb.name = 'London Road'
 -- 7.
-
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company =b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Haymarket' AND stopb.name='Leith'
 -- 8. - Skip this one
 
 -- 9.
-
+SELECT stopa.name, a.company, a.num
+FROM route a
+  JOIN route b ON (a.num=b.num AND a.company=b.company)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopb.name = 'Craiglockhart'
 -- 10.
-
+SELECT DISTINCT a.num, a.company, stopb.name ,  c.num,  c.company
+FROM route a JOIN route b
+ON (a.company = b.company AND a.num = b.num)
+JOIN ( route c JOIN route d ON (c.company = d.company AND c.num= d.num))
+JOIN stops stopa ON (a.stop = stopa.id)
+JOIN stops stopb ON (b.stop = stopb.id)
+JOIN stops stopc ON (c.stop = stopc.id)
+JOIN stops stopd ON (d.stop = stopd.id)
+WHERE  stopa.name = 'Craiglockhart' AND stopd.name = 'Sighthill'
+            AND  stopb.name = stopc.name
+ORDER BY LENGTH(a.num), b.num, stopb.id, LENGTH(c.num), d.num
 -- 11. - Skip this one
 
 -- 12. - Skip this one
@@ -29,35 +66,82 @@
 
 -- 7 More JOIN operations
 -- 1.
-
+SELECT id, title
+FROM movie
+WHERE yr=1962
 -- 2.
-
+SELECT yr
+FROM movie
+WHERE title = 'Citizen Kane'
 -- 3.
-
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE '%star trek%'
+ORDER BY yr
 -- 4.
-
+SELECT title
+FROM movie
+WHERE id IN ( 11768, 11955, 21191)
 -- 5.
-
+SELECT id
+FROM actor
+WHERE name = 'Glenn Close'
 -- 6.
-
+SELECT id
+FROM movie
+WHERE title = 'Casablanca'
 -- 7.
-
+SELECT name
+FROM actor, casting
+WHERE id=actorid AND movieid = (SELECT id FROM movie WHERE title = 'Casablanca')
 -- 8.
-
+SELECT name
+FROM actor
+  JOIN casting ON (id=actorid AND movieid = (SELECT id FROM movie WHERE title = 'Alien'))
 -- 9.
-
+SELECT title
+FROM movie
+  JOIN casting ON (id=movieid AND actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford'))
 -- 10.
-
+SELECT title
+FROM movie
+    JOIN casting ON (id=movieid AND actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford') AND ord != 1)
 -- 11.
-
+SELECT title, name
+FROM movie JOIN casting ON (id=movieid)
+JOIN actor ON (actor.id = actorid)
+WHERE ord=1 AND  yr = 1962
 -- 12.
-
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+         JOIN actor   ON actorid=actor.id
+WHERE name='John Travolta'
+GROUP BY yr
+HAVING COUNT(title)=(SELECT MAX(c) FROM
+(SELECT yr,COUNT(title) AS c FROM
+   movie JOIN casting ON movie.id=movieid
+         JOIN actor   ON actorid=actor.id
+ WHERE name='John Travolta'
+ GROUP BY yr) AS t)
 -- 13.
-
+SELECT title, name FROM movie
+JOIN casting x ON movie.id = movieid
+JOIN actor ON actor.id =actorid
+WHERE ord=1 AND movieid IN
+(SELECT movieid FROM casting y
+JOIN actor ON actor.id=actorid
+WHERE name='Julie Andrews')
 -- 14.
-
+SELECT name
+FROM actor
+  JOIN casting ON (id = actorid AND (SELECT COUNT(ord) FROM casting WHERE actorid = actor.id AND ord=1)>=30)
+GROUP BY name
 -- 15.
-
+SELECT title, COUNT(actorid) as cast
+FROM movie JOIN casting on id=movieid
+WHERE yr = 1978
+GROUP BY title
+ORDER BY cast DESC
 
 -- 8 Using Null
 -- 1.
