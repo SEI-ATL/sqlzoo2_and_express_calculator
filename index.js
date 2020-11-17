14a20015e5f86dfb15af5c7120ecfa37692852a6
Tu Hoa Huynh <tuhoa.huynh@yahoo.com>
2020-11-16 21:24:45 -0500

    [ejs] Built out root route in index.ejs and layouts.ejs

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ const app = express();
 const ejsLayouts = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs');
-app.use(ejsLayouts)
+app.use(ejsLayouts);
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+
+app.listen(8000, () => {
+    console.log('server has started')
+})
