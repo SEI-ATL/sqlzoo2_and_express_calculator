e04c7bbdaaa1cd04dabcd8639389457e17a7061b
Jeremy Uriz <jeremyuriz@Jennifers-Air.attlocal.net>
2020-11-17 09:31:21 -0500

    [controllers, views] Refactor sep. of concerns

--- controllers/mathController.js
+++ controllers/mathController.js
@@ -2,12 +2,12 @@ const mathController = require('express').Router()
 
 mathController.get('/double/:number', (req, res) => {
     const userNumber = req.params.number
-    res.render('math/double', { number: userNumber })
+    res.render('math/double', { number: userNumber, doubleNumber: userNumber * 2 })
 })
 
 mathController.get('/square/:number', (req, res) => {
     const userNumber = req.params.number
-    res.render('math/square', { number: userNumber })
+    res.render('math/square', { number: userNumber, squareNumber: userNumber * userNumber })
 })
 
 module.exports = mathController;
--- controllers/stringController.js
+++ controllers/stringController.js
@@ -2,12 +2,12 @@ const stringController = require('express').Router()
 
 stringController.get('/reverse/:string', (req, res) => {
     const userString = req.params.string
-    res.render('strings/reverse', { string: userString })
+    res.render('strings/reverse', { plainString: userString, reversedString: userString.split('').reverse().join('')} )
 })
 
 stringController.get('/upcase/:string', (req, res) => {
     const userString = req.params.string
-    res.render('strings/upcase', { string: userString })
+    res.render('strings/upcase', { plainString: userString, upperString: userString.toUpperCase() })
 })
 
 module.exports = stringController;
