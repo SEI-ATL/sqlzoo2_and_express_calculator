b6ec4ad857890402d29890938dd958184f8e2011
Andrew McManus <andrewmcmanusmusic@gmail.com>
2020-11-17 08:51:43 -0500

    sqlzoo done, calculator incomplete

--- express_calculator/index.js
+++ express_calculator/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 const ejsLayouts = require('express-ejs-layouts')
-const mathController = require('./math/mathController')
-const stringsController = require('./strings/stringsController')
+const mathController = require('./controllers/mathController')
+const stringController = require('./controllers/stringController')
 
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
@@ -10,6 +10,10 @@ app.use(ejsLayouts)
 app.get('/', function(req, res) {
     res.render('home');
   });
+
+//   app.get('/animals', function(req, res) {
+//     res.render('animals', {title: 'Favorite Animals', animals: ['sand crab', 'corny joke dog']})
+//   });
 
 app.get('/math/double/:number', (req, res) => { // this makes the /blog page irrelevant without a date
     const theNumber = req.params.number;
@@ -20,6 +24,10 @@ app.get('/strings/:string', (req, res) => { // this makes the /blog page irrelev
     const theString = req.params.string;
     res.render('blog', { string: theString }); //
 });
+// res.send("Our parameter is " + req.params.input + ".");
+
+app.use('/math', mathController)
+app.use('/strings', stringController)
 
 app.listen(8000, () => {
     console.log('server started!');
